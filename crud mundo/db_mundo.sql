cde9b2e728d390650c0ed98a21edfe7f6b44e076
GuiLeo016 <guilherme221208@gmail.com>
2026-06-08 15:19:04 -0300

    08/06/26 15:19
    Desenvolvimento do Banco de Dados
    Início do desenvolvimento da homepage da aplicação web

---
+++ crud mundo/db_mundo.sql
@@ -0,0 +1,48 @@
+create database db_mundo;
+use db_mundo;
+
+create table tb_continentes(
+	id_continente INT PRIMARY KEY AUTO_INCREMENT,
+    nome_continente VARCHAR(45) NOT NULL,
+    populacao_continente INT NOT NULL,
+    area_continente DECIMAL(12,3) NOT NULL,
+    total_paises INT NOT NULL
+);
+
+create table tb_governantes(
+	id_governante INT PRIMARY KEY AUTO_INCREMENT,
+	nome_governante VARCHAR(80) NOT NULL,
+    partido_politico VARCHAR(80) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    idade INT NOT NULL,
+    data_inicio_mandato DATE NOT NULL,
+    data_final_mandato DATE NOT NULL
+);
+
+create table tb_paises(
+	id_pais INT PRIMARY KEY AUTO_INCREMENT,
+    nome_pais VARCHAR(50) NOT NULL,
+    populacao_pais INT NOT NULL,
+    area_pais DECIMAL(12,3),
+    idioma VARCHAR(20) NOT NULL,
+    clima_pais VARCHAR(20) NOT NULL,
+    regime_politico VARCHAR(40) NOT NULL,
+    moeda VARCHAR(25) NOT NULL,
+    governante INT NOT NULL,
+    continente_pais INT NOT NULL,
+    
+    FOREIGN KEY (governante) REFERENCES tb_governantes (id_governante),
+    FOREIGN KEY (continente_pais) REFERENCES tb_continente (id_continente)
+);
+
+create table tb_cidades(
+	id_cidade INT PRIMARY KEY AUTO_INCREMENT,
+    nome_cidade VARCHAR(80) NOT NULL,
+    populacao_cidade INT NOT NULL,
+    area_cidade DECIMAL(12,3) NOT NULL,
+    clima_cidade VARCHAR(20) NOT NULL,
+    data_fundacao DATE NOT NULL,
+    governante_cidade INT NOT NULL,
+    
+    FOREIGN KEY (governante_cidade) REFERENCES tb_governantes (id_governante)
+);
---
+++ crud mundo/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>CRUD Mundo</title>
+</head>
+<body>
+    <div id="main">
+        <div id="header">
+            <ul>
+                <li><a href="">Início</a></li>
+                <li><a href="">Continentes</a></li>
+                <li><a href="">Países</a></li>
+                <li><a href="">Cidades</a></li>
+                <li><a href="">Governantes</a></li>
+            </ul>
+        </div>
+        <div id="container">
+            <section></section>
+            <h1>texto</h1>
+        </div>
+    </div>
+</body>
+</html>
